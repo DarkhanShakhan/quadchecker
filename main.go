b9bf00546b25b2b2cc63a42c3994fde4b3afa9da
Dara <darkhan.shakhanov@gmail.com>
2022-05-08 18:19:03 +0600

    commit 2

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := reader.ReadString('\t')
+	length := len(name)
+	columns := 0
+	for _, v := range name {
+		if v == '\n' {
+			break
+		}
+		columns++
+	}
+	rows := length / columns
+	fmt.Println("Columns: ", columns)
+	fmt.Println("Rows: ", rows)
 }
