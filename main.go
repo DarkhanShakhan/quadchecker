7124ab5f6fae593e6e487a61871b3accb8214377
Dara <darkhan.shakhanov@gmail.com>
2022-05-08 18:38:31 +0600

    commit 3

--- main.go
+++ main.go
@@ -18,6 +18,46 @@ func main() {
 		columns++
 	}
 	rows := length / columns
-	fmt.Println("Columns: ", columns)
-	fmt.Println("Rows: ", rows)
+	fmt.Println(name == quadA(rows, columns))
+	fmt.Println(name == quadB(rows, columns))
+}
+
+func quadA(h, v int) string {
+	result := ""
+	for i := 1; i <= h; i++ {
+		for j := 1; j <= v; j++ {
+			if i == 1 && j == 1 || i == 1 && j == v || i == h && j == v || i == h && j == 1 {
+				result += "o"
+			} else if i == 1 || i == h {
+				result += "-"
+			} else if j == 1 || j == v {
+				result += "|"
+			} else {
+				result += " "
+			}
+		}
+		result += "\n"
+	}
+	return result
+}
+
+func quadB(h, v int) string {
+	result := ""
+	for i := 1; i <= h; i++ {
+		for j := 1; j <= v; j++ {
+			if i == 1 && j == 1 {
+				result += "/"
+			} else if i == 1 && j == v || i == h && j == 1 {
+				result += "\\"
+			} else if i == h && j == v {
+				result += "/"
+			} else if i == 1 || i == h || j == 1 || j == v {
+				result += "*"
+			} else {
+				result += " "
+			}
+		}
+		result += "\n"
+	}
+	return result
 }
